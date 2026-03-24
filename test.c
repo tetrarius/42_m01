344c208d0564d65a3cb412ee1427608bb105ad4f
Artak Avakian <aravakia@c2r15s4.42yerevan.am>
2026-03-24 16:13:26 +0400

    24-3

--- test.c
+++ test.c
@@ -6,30 +6,27 @@ int	main(void)
     int	ft_ret;
     int	std_ret;
 
-    printf("=== ТЕСТ 1: NULL строка (%%s) ===\n");
-    // Оригинальный printf на Linux печатает "(null)", проверим твой!
+    printf("=== TEST 1: NULL string (%%s) ===\n");
     ft_ret = ft_printf("FT : %s\n", (char *)NULL);
     std_ret = printf("STD: %s\n", (char *)NULL);
     printf("Return -> FT: %d | STD: %d\n\n", ft_ret, std_ret);
 
-    printf("=== ТЕСТ 2: NULL указатель (%%p) ===\n");
-    // Должен напечатать "(nil)" на Linux
+    printf("=== TEST 2: NULL pointer (%%p) ===\n");
     ft_ret = ft_printf("FT : %p\n", NULL);
     std_ret = printf("STD: %p\n", NULL);
     printf("Return -> FT: %d | STD: %d\n\n", ft_ret, std_ret);
 
-    printf("=== ТЕСТ 3: Ноль в HEX (%%x и %%X) ===\n");
-    // Должен вывести просто "0", а не пустую строку
+    printf("=== TEST 3: Zero in HEX (%%x и %%X) ===\n");
     ft_ret = ft_printf("FT : %x %X\n", 0, 0);
     std_ret = printf("STD: %x %X\n", 0, 0);
     printf("Return -> FT: %d | STD: %d\n\n", ft_ret, std_ret);
 
-    printf("=== ТЕСТ 4: Подряд идущие проценты (%%%%) ===\n");
-    ft_ret = ft_printf("FT : %%%%%%\n"); // Ожидаем печать трех знаков %
+    printf("=== TEST 4: prcent in line (%%%%) ===\n");
+    ft_ret = ft_printf("FT : %%%%%%\n");
     std_ret = printf("STD: %%%%%%\n");
     printf("Return -> FT: %d | STD: %d\n\n", ft_ret, std_ret);
 
-    printf("=== ТЕСТ 5: Мульти-комбо ===\n");
+    printf("=== TEST 5: multi combo ===\n");
     ft_ret = ft_printf("FT : %c %s %p %d %i %u %x %X %%\n", 'A', "42", (void *)0x42, 42, -42, 42, 42, 42);
     std_ret = printf("STD: %c %s %p %d %i %u %x %X %%\n", 'A', "42", (void *)0x42, 42, -42, 42, 42, 42);
     printf("Return -> FT: %d | STD: %d\n\n", ft_ret, std_ret);
